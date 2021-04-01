7780e7b37a14cdd44e376852e14540721ae12438
klamb95 <klamb1995@gmail.com>
2021-04-01 12:40:19 +0100

    first task done

--- task_list.py
+++ task_list.py
@@ -1,3 +1,5 @@
+#import pdb
+
 tasks = [
     { "description": "Wash Dishes", "completed": False, "time_taken": 10 },
     { "description": "Clean Windows", "completed": False, "time_taken": 15 },
@@ -6,3 +8,11 @@ tasks = [
     { "description": "Walk Dog", "completed": True, "time_taken": 60 },
 ]
 
+def umcompleted_tasks(list):
+    #pdb.set_trace()
+    uncompleted_task = []
+    for task in list:
+        if task["completed"] == False:
+            uncompleted_task.append(task)
+    return uncompleted_task
+    
